ecde767b3de8e4cc547da911f0b24d1c212931be
Freedom Loisa <kitakayaloisa@gmail.com>
2022-10-25 07:22:52 +0300

    fetch candidate avatar

---
+++ src/components/Organizer-console/Candidate-modals/CandidateAvatar.jsx
@@ -0,0 +1,32 @@
+import { gql, useQuery } from "@apollo/client";
+
+const CANDIDATE_AVATAR = gql`
+  query CandidateAvatar($id: String!) {
+    candidateAvatar(id: $id)
+  }
+`;
+
+const CandidateAvatar = ({ CandidateID }) => {
+  const { loading, error, data } = useQuery(CANDIDATE_AVATAR, {
+    variables: { id: CandidateID },
+  });
+
+  if (data) {
+    console.log(data);
+    console.log("Data fetched successfully.");
+  }
+  if (loading) return "Fetching...";
+  if (error) return `Fetching error! ${error.message}`;
+
+  return (
+    <div>
+      <img
+        className="w-full h-full rounded-t-lg"
+        src={data.candidateAvatar}
+        alt="candidate avatar"
+      />
+    </div>
+  );
+};
+
+export default CandidateAvatar;
