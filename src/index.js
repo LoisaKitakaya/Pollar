84a896dfe91516a99c7efb457d7ff0dd3b2d7446
Freedom Loisa <kitakayaloisa@gmail.com>
2022-10-30 08:09:39 +0300

    cleaning code on index.jsx

--- src/index.js
+++ src/index.js
@@ -21,9 +21,7 @@ const httpLink = createHttpLink({
 });
 
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
   const token = localStorage.getItem("token");
-  // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
@@ -58,7 +56,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
